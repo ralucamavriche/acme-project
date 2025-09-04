d9d3914658adbc5d7d3a99a8798daf6729befb49
ralucamavriche <ralucamavriche@gmail.com>
2025-09-04 15:11:47 +0300

    fix(AC-1): implement DashboardLayout with header and outlet (#1)

--- src/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,5 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const DashboardLayout: React.FC = () => {
-  return <div>DashboardLayout</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-blue-800 text-white p-4">Dashboard Header</header>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default DashboardLayout;
