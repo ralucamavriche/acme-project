89835d879135963740f88c5040b10874b9bd5a01
ralucamavriche <ralucamavriche@gmail.com>
2025-09-05 18:14:32 +0300

    feat: add hero images and update Home page layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,34 @@
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import React from 'react';
-import Button from '../../components/Button';
 
 const Home: React.FC = () => (
-  <div className="p-8">
-    <h1 className="text-3xl font-bold">Home Page</h1>
-    <Button onClick={() => alert('cliked')}>Click Me</Button>
+  <div className="flex flex-col md:flex-row gap-4 mt-4 ">
+    <div className="flex flex-col gap-6 justify-center bg-gray-50 rounded-lg px-6 py-10 w-full md:p-20 md:w-2/5 ">
+      <p className="text-xl md:text-3xl text-gray-800 md:leading-normal">
+        <strong>Welcome to Acme. </strong>
+        This is the example for the
+        <a className="text-blue-500" href="https://nextjs.org/learn">
+          {' '}
+          Next.js Learn Course
+        </a>
+        , brought to you by Vercel.
+      </p>
+      <button className="flex items-center gap-5 self-start text-sm rounded-lg bg-primary px-6 py-3 font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+        Log in <ArrowRightIcon className="w-5 md:w-6" />
+      </button>
+    </div>
+    <div className="flex justify-center items-center w-full md:w-3/5 px-6 py-3 md:px-28 md:py-12">
+      <img
+        src="/hero-desktop.png"
+        alt="Hero Desktop"
+        className="w-full max-w-[1000px] h-auto hidden md:block"
+      />
+      <img
+        src="/hero-mobile.png"
+        alt="Hero Mobile"
+        className="block md:hidden w-full max-w-[560px] h-auto"
+      />
+    </div>
   </div>
 );
 
