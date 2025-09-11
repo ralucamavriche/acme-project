fc70ec3aa7e476c69d2ba43575992851249a46fe
ralucamavriche <ralucamavriche@gmail.com>
2025-09-11 16:33:14 +0300

    fix(Stats): correct icon class name and ensure proper key usage in Card component

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -34,7 +34,7 @@ const Card: React.FC<CardProps> = ({ title, amount, Icon: Icon }) => {
   return (
     <div key={title} className="rounded-xl bg-gray-50 p-2">
       <div className="flex p-4">
-        <Icon className="text-gray-70 h-5 w-5" />
+        <Icon className="h-5 w-5 text-gray-700" />
         <h1 className="ml-2 font-medium">{title}</h1>
       </div>
       <h1 className="rounded-xl bg-white px-4 py-8 text-center text-2xl">{amount}</h1>
@@ -46,7 +46,9 @@ const Stats = () => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
       {statsData.map(({ title, amount, Icon: Icon }) => (
-        <Card key={title} title={title} amount={amount} Icon={Icon} />
+        <div key={title}>
+          <Card title={title} amount={amount} Icon={Icon} />
+        </div>
       ))}
     </div>
   );
